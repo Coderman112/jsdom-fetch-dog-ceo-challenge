b2d2eadc265702d391a17bf6e0072f993ee67e5b
ChristianCudby <christiancudby@hotmail.com>
2021-04-28 18:25:31 -0400

    commit tree 51b150b308d4cfbec1b259cb9610d5e121bc75e1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 console.log('%c HI', 'color: firebrick')
+function fetchImages() {
+    // To pass the tests, don't forget to return your fetch!
+    // fetch('https://anapioficeandfire.com/api/books')
+    // .then(object => renderBooks(books));
+    return fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(resp => resp.json())
+    .then(object => renderBooks(object))
+}
+  
+function renderImage(books) {
+    const main = document.querySelector('main');
+    books.forEach(book => {
+      const h2 = document.createElement('h2');
+      h2.innerHTML = book.name;
+      main.appendChild(h2);
+    });
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchImages();
+  });
